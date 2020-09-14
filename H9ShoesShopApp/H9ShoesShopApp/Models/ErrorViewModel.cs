ad612c7461847663918b7b8d418e7bd868524185
huyhieu <lehuyhieu1099@gmail.com>
2020-09-14 17:00:57 +0700

    Huy Hieu_Init Project

---
+++ H9ShoesShopApp/H9ShoesShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace H9ShoesShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
